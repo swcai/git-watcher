f7e959caacd26561a5f1bd21875690f8e66e7009
Wenwei Cai <stanley.w.cai@gmail.com>
2012-11-07 09:54:25 +0800

    First branch-out for github commit
    Signed-off-by: Wenwei Cai <stanley.w.cai@gmail.com>

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ PROJECT_NAME = "TeamEventApp"
 PROJECT_DIR = os.getcwd()
 PROJECT_SRC_DIR = "%s/src/TeamEventApp" % PROJECT_DIR
 PROJECT_LOG_DIR = "%s/log" % PROJECT_DIR
-PROJECT_ADMIN ='stanley.w.cai@intel.com'
-PROJECT_TO_LIST = ['stanley.w.cai@intel.com', 'yong.hu@intel.com', 'fanjiang.pei@intel.com', 'junyong.liang@intel.com', 'shidong.ren@intel.com', 'qinghui.jian@intel.com', 'kaining.yuan@intel.com', 'jun.feng.lu@intel.com']
+PROJECT_ADMIN = 'stanley.w.cai@gmail.com'
+PROJECT_TO_LIST = ['stanley.w.cai@gmail.com']
+PROJECT_EMAIL_SUBJECT = ''
 
--- main.py
+++ main.py
@@ -10,8 +10,10 @@ import daemon
 import config
 from email.mime.text import MIMEText
 
+
 def run_without_output(cmd):
    os.popen(cmd)
+
 
 def run_with_output(cmd):
    with os.popen(cmd) as f:
@@ -20,16 +22,19 @@ def run_with_output(cmd):
          yield line
          line = f.readline()
 
+
 def send_mail(commit, desc, email):
-   msg = MIMEText(''.join(run_with_output('git log -1 %s' % commit)))
-   msg['subject'] = "New commit for %s project from %s" % (config.PROJECT_NAME, email)
+   msg['subject'] = config.PROJECT_EMAIL_SUBJECT or "New commit for %s project from %s" % (config.PROJECT_NAME, email)
+   body = ''.join(run_with_output('git log -1 %s' % commit))
+   msg = MIMEText(body)
    try:
-      s = smtplib.SMTP()
+      s= smtplib.SMTP()
       s.connect("localhost")
       s.sendmail(config.PROJECT_ADMIN, config.PROJECT_TO_LIST, msg.as_string())
       s.close()
    except Exception, e:
       print str(e)
+
 
 class checker:
    last_commit = ''
@@ -37,15 +42,18 @@ class checker:
    def init(self):
       self.fetch()
 
+
    def fetch(self):
       cmd = 'git fetch origin'
       run_without_output(cmd)
+
 
    def commits_since_lasttime(self):
       cmd = 'git log origin/master ' + self.last_commit + '.. --format="%H__SPLIT__%s__SPLIT__%ae"'
       self.commits = map(lambda line: line.rstrip().split('__SPLIT__'), run_with_output(cmd))
       if len(self.commits) > 0:
          self.last_commit = self.commits[0][0]
+
 
    def check_and_send_new_commits(self):
       pwd = os.getcwd()
@@ -54,17 +62,22 @@ class checker:
       if self.last_commit != '':
          self.commits_since_lasttime()
          for commit in self.commits:
+            print commit
             send_mail(*commit)
       else:
          cmd = 'git log -l origin/master --format="%H"'
-         self.last_commit = run_with_output(cmd).next()
+         self.last_commit = run_with_output(cmd).next().rstrip()
+         print self.last_commit
+
       os.chdir(pwd) 
+
 
 def main():
    instance = checker()
    while True:
       instance.check_and_send_new_commits()
       time.sleep(30)
+
 
 if __name__ == '__main__':
    with daemon.DaemonContext():
